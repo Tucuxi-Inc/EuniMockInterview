b9645595aeef4425b40505b482aead2bb8d193d0
Kevin-Tucuxi <kevin@tucuxi.ai>
2025-04-03 17:56:51 -0700

    Initial Commit

---
+++ EuniMockInterviewTests/EuniMockInterviewTests.swift
@@ -0,0 +1,17 @@
+//
+//  EuniMockInterviewTests.swift
+//  EuniMockInterviewTests
+//
+//  Created by Kevin Keller on 4/3/25.
+//
+
+import Testing
+@testable import EuniMockInterview
+
+struct EuniMockInterviewTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
